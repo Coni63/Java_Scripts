564b7a94f2f9f7d31e94bf6ed5a7e78dfe16c566
= <=>
2018-08-06 18:01:45 +0200

    template with Maven

---
+++ maven_test/src/fr/coni/maven_test/Main.java
@@ -0,0 +1,28 @@
+package fr.coni.maven_test;
+
+import org.jbox2d.common.*;
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	static float FrameRate = 15f;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("fr.coni.code_finder.Main");
+	}
+	
+	public void settings(){
+	    size(800, 450);
+	}
+
+
+	public void setup() {
+		frameRate(FrameRate);
+	}
+	
+	public void draw() {
+		
+	}
+
+}
